eacbe879cbfe5480115c29fb44ddbd438c944cec
gconeice <yyang811@gatech.edu>
2024-10-19 17:53:11 -0700

    libot library add

---
+++ oprf/util/libot-pre.h
@@ -0,0 +1,106 @@
+#ifndef LIBOT_PRE_H
+#define LIBOT_PRE_H
+
+#include "coproto/Socket/AsioSocket.h"
+#include "libOTe/Base/SimplestOT.h"
+#include "libOTe/Base/MasnyRindal.h"
+#include "libOTe/Base/MasnyRindalKyber.h"
+#include "libOTe/TwoChooseOne/SoftSpokenOT/SoftSpokenShOtExt.h"
+#include "libOTe/TwoChooseOne/SoftSpokenOT/SoftSpokenMalOtExt.h"
+#include <vector>
+#include <array>
+#include <span>
+
+namespace libotpre{
+
+bool is_malicious = false;
+bool is_pq = false;
+
+void set_malicious() { 
+    is_malicious = true; 
+}
+
+void set_pq() {
+    is_pq = true;
+}
+
+void preot_sender(size_t numOTs, std::span<std::array<osuCrypto::block, 2>> sMsgs, osuCrypto::Socket& sock) {
+    if (is_malicious == false && is_pq == false) {
+        osuCrypto::SoftSpokenShOtSender<> sender;
+        osuCrypto::MasnyRindal bot;
+        osuCrypto::PRNG prng(osuCrypto::sysRandomSeed());
+
+        // setup base OTs
+        osuCrypto::u64 baseOTs = sender.baseOtCount();
+        std::vector<osuCrypto::block> rMsgBase(baseOTs);
+        osuCrypto::BitVector choices(baseOTs);
+        choices.randomize(prng);
+        coproto::sync_wait(bot.receive(choices, rMsgBase, prng, sock));
+        coproto::sync_wait(sock.flush());
+        sender.setBaseOts(rMsgBase, choices);
+
+        // OT extension
+        coproto::sync_wait(sender.send(sMsgs, prng, sock));
+        coproto::sync_wait(sock.flush());
+    } else if (is_malicious == true && is_pq == false) {
+        osuCrypto::SoftSpokenMalOtSender sender;
+        osuCrypto::MasnyRindal bot;
+        osuCrypto::PRNG prng(osuCrypto::sysRandomSeed());
+
+        // setup base OTs
+        osuCrypto::u64 baseOTs = sender.baseOtCount();
+        std::vector<osuCrypto::block> rMsgBase(baseOTs);
+        osuCrypto::BitVector choices(baseOTs);
+        choices.randomize(prng);
+        coproto::sync_wait(bot.receive(choices, rMsgBase, prng, sock));
+        coproto::sync_wait(sock.flush());
+        sender.setBaseOts(rMsgBase, choices);
+
+        // OT extension
+        coproto::sync_wait(sender.send(sMsgs, prng, sock));
+        coproto::sync_wait(sock.flush());
+    }    
+}
+
+void preot_receiver(size_t numOTs, osuCrypto::BitVector &choices, std::span<osuCrypto::block> rMsgs, osuCrypto::Socket& sock) {
+    if (is_malicious == false && is_pq == false) {
+        osuCrypto::SoftSpokenShOtReceiver<> receiver;
+        osuCrypto::MasnyRindal bot;
+        osuCrypto::PRNG prng(osuCrypto::sysRandomSeed());
+
+        // setup base OTs
+        osuCrypto::u64 baseOTs = receiver.baseOtCount();
+        std::vector<std::array<osuCrypto::block, 2>> sMsgBase(baseOTs);
+        coproto::sync_wait(bot.send(sMsgBase, prng, sock));
+        coproto::sync_wait(sock.flush());
+        receiver.setBaseOts(sMsgBase);
+
+        // OT extension
+        choices.randomize(prng);
+        coproto::sync_wait(receiver.receive(choices, rMsgs, prng, sock));
+        coproto::sync_wait(sock.flush());
+    } else if (is_malicious == true && is_pq == false) {
+        osuCrypto::SoftSpokenMalOtReceiver receiver;
+        osuCrypto::MasnyRindal bot;
+        osuCrypto::PRNG prng(osuCrypto::sysRandomSeed());
+
+        // setup base OTs
+        osuCrypto::u64 baseOTs = receiver.baseOtCount();
+        std::vector<std::array<osuCrypto::block, 2>> sMsgBase(baseOTs);
+        coproto::sync_wait(bot.send(sMsgBase, prng, sock));
+        coproto::sync_wait(sock.flush());
+        receiver.setBaseOts(sMsgBase);
+
+        // OT extension
+        choices.randomize(prng);
+        coproto::sync_wait(receiver.receive(choices, rMsgs, prng, sock));
+        coproto::sync_wait(sock.flush());        
+    }
+}
+
+};
+
+
+
+
+#endif
--- test/oprf/try.cpp
+++ test/oprf/try.cpp
@@ -16,6 +16,11 @@
 #include "cryptoTools/Network/IOService.h"
 #include "libOTe/TwoChooseOne/Silent/SilentOtExtSender.h"
 #include "libOTe/TwoChooseOne/Silent/SilentOtExtReceiver.h"
+#include "libOTe/TwoChooseOne/SoftSpokenOT/SoftSpokenShOtExt.h"
+#include "libOTe/TwoChooseOne/SoftSpokenOT/SoftSpokenMalOtExt.h"
+
+
+#include "oprf/util/libot-pre.h"
 
 
 #include <iostream>
@@ -37,52 +42,85 @@ int main(int argc, char **argv) {
 
   int party = std::atoi(argv[1]);
 
+  //libotpre::set_malicious();
+
   if (party == 1) {
 
     auto sock = osuCrypto::cp::asioConnect("127.0.0.1:12345", true); 
-    //osuCrypto::SimplestOT xxx;
-    osuCrypto::MasnyRindal xxx;
+    // osuCrypto::SoftSpokenShOtReceiver extreceiver;
+    // osuCrypto::SimplestOT xxx;
+    // osuCrypto::SoftSpokenMalOtReceiver extreceiver;
+    // osuCrypto::MasnyRindal xxx;
     
     //osuCrypto::setThreadName("Sender");
-    osuCrypto::PRNG prng(osuCrypto::sysRandomSeed());
+    // osuCrypto::PRNG prng(osuCrypto::sysRandomSeed());
 
-    osuCrypto::u64 numOTs = 5;
-    std::vector<std::array<osuCrypto::block, 2>> sendMsg(numOTs);
+    // osuCrypto::u64 numOTs = extreceiver.baseOtCount();
+    // std::vector<std::array<osuCrypto::block, 2>> sendMsg(numOTs);
 
-    auto p0 = xxx.send(sendMsg, prng, sock);
-    coproto::sync_wait(p0);
-    coproto::sync_wait(sock.flush());
+    // auto p0 = xxx.send(sendMsg, prng, sock);
+    // coproto::sync_wait(p0);
+    // coproto::sync_wait(sock.flush());
 
-    for (int i = 0; i < numOTs; i++) {
+    // extreceiver.setBaseOts(sendMsg);
+    // std::cout << "comm: " << sock.bytesReceived()+sock.bytesSent() << std::endl;
+
+    int extotnum = 384;
+    osuCrypto::BitVector extchoices(extotnum);
+    // extchoices.randomize(prng);
+    osuCrypto::AlignedUnVector<osuCrypto::block> rMsgs(extotnum);
+    // coproto::sync_wait(extreceiver.receive(extchoices, rMsgs, prng, sock));
+    // coproto::sync_wait(sock.flush());
+
+    libotpre::preot_receiver(extotnum, extchoices, rMsgs, sock);
+
+    for (int i = 0; i < 10; i++) {      
       std::cout << i << ':' << std::endl;
-      std::cout << sendMsg[i][0] << std::endl << sendMsg[i][1] << std::endl;
-    }
+      std::cout << extchoices[i] << ' ' << rMsgs[i] << std::endl;
+    }    
+
     std::cout << "comm: " << sock.bytesReceived()+sock.bytesSent() << std::endl;
 
   } else {
 
     auto sock = osuCrypto::cp::asioConnect("127.0.0.1:12345", false);
-    //osuCrypto::SimplestOT xxx;
-    osuCrypto::MasnyRindal xxx;
+    // osuCrypto::SoftSpokenShOtSender extsender;
+    // osuCrypto::SimplestOT xxx;
+    //osuCrypto::SoftSpokenMalOtSender extsender;
+    //osuCrypto::MasnyRindal xxx;
+    
 
     //osuCrypto::setThreadName("Sender");
-    osuCrypto::PRNG prng(osuCrypto::sysRandomSeed());
+    // osuCrypto::PRNG prng(osuCrypto::sysRandomSeed());
 
-    osuCrypto::u64 numOTs = 5;
-    std::vector<osuCrypto::block> recvMsg(numOTs);
-    osuCrypto::BitVector choices(numOTs);
-    choices.randomize(prng);
+    // osuCrypto::u64 numOTs = extsender.baseOtCount();
+    // std::vector<osuCrypto::block> recvMsg(numOTs);
+    // osuCrypto::BitVector choices(numOTs);
+    // choices.randomize(prng);
 
-    auto p1 = xxx.receive(choices, recvMsg, prng, sock);
-    coproto::sync_wait(p1);
-    coproto::sync_wait(sock.flush());
+    // auto p1 = xxx.receive(choices, recvMsg, prng, sock);
+    // coproto::sync_wait(p1);
+    // coproto::sync_wait(sock.flush());
 
-    for (int i = 0; i < numOTs; i++) {      
+    // extsender.setBaseOts(recvMsg, choices);
+    // std::cout << "comm: " << sock.bytesReceived()+sock.bytesSent() << std::endl;
+
+    int extotnum = 384;
+    osuCrypto::AlignedUnVector<std::array<osuCrypto::block, 2>> sMsgs(extotnum);
+    // coproto::sync_wait(extsender.send(sMsgs, prng, sock));
+    // coproto::sync_wait(sock.flush());
+
+    libotpre::preot_sender(extotnum, sMsgs, sock);
+
+
+    for (int i = 0; i < 10; i++) {
       std::cout << i << ':' << std::endl;
-      std::cout << choices[i] << ' ' << recvMsg[i] << std::endl;
+      std::cout << sMsgs[i][0] << std::endl << sMsgs[i][1] << std::endl;
     }
-    std::cout << "comm: " << sock.bytesReceived()+sock.bytesSent() << std::endl;
 
+
+
+    std::cout << "comm: " << sock.bytesReceived()+sock.bytesSent() << std::endl;
 
   }
 
